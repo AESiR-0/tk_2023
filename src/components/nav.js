59c5ff5b6cd3d4caa357d0822c0190ad0ce79e95
AESiR <cyborgkiller1008@gmail.com>
2023-12-30 17:24:42 +0530

    respo

--- src/components/nav.js
+++ src/components/nav.js
@@ -11,8 +11,8 @@ const Nav = () => {
   ];
   let [open,setOpen]=useState(false);
 return (
-  <div className='shadow-md w-full relative top-0 left-0'>
-    <div className='md:flex relative items-center justify-around bg-white py-4 md:px-10 px-7'>
+  <div className='shadow-md w-full  top-0 left-0'>
+    <div className='md:flex  items-center justify-around bg-white py-4 md:px-10 px-7'>
     <div className='font-bold text-2xl cursor-pointer flex items-center  
     text-gray-800'>
       <span className='text-4xl flex justify-center items-center gap-4  mr-1 pt-2'>
@@ -26,7 +26,7 @@ return (
     <FaBars/>
     </div>
 
-    <ul className={`md:flex md:items-center md:pb-0 pb-12 bg-white absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+    <ul className={`md:flex md:items-center md:pb-0 pb-12 bg-white absolute md:static bg-white  left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
       {
         Links.map((link)=>(
           <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
